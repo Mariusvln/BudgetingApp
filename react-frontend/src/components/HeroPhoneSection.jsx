6eca875009ac27bd3b7ee4d2e34c2b1ee6eec64a
mariusvln <mariusvlnn@gmail.com>
2026-03-19 12:12:22 +0200

    Added phone section

---
+++ react-frontend/src/components/HeroPhoneSection.jsx
@@ -0,0 +1,11 @@
+import phone1 from '../assets/phone1.png'
+
+function HeroPhoneSection() {
+  return (
+    <section className="hero-phone-section">
+      <img src={phone1} alt="Phone" />
+    </section>
+  );
+}
+
+export default HeroPhoneSection
