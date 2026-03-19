80644c7d5f50ff85c4a250e46783e3b0d7ca3050
Daniil <daniil.batiuskov@gmail.com>
2026-03-19 13:05:50 +0200

    fix

--- react-frontend/src/components/HeroMaster.jsx
+++ react-frontend/src/components/HeroMaster.jsx
@@ -2,21 +2,21 @@ function HeroMaster() {
   return (
     <>
       <div className="flex justify-center">
-        <p className="text-center p-2 text-3xl w-125 mb-5 bg-[#DCFCE7] text-[#15803D] rounded-3xl uppercase">trusted by over 100K+ users</p>
+        <p className="text-center p-2 w-65 mt-27 mb-4 bg-[#DCFCE7] text-[#15803D] rounded-3xl uppercase">trusted by over 100K+ users</p>
       </div>
 
-      <div className="text-center mb-10">
-        <h1 className="text-8xl font-bold mb-3">Master Your Money with</h1>
-        <h1 class="text-8xl bg-gradient-to-r from-[#22C55E] to-[#15803D] bg-clip-text text-transparent font-bold">
+      <div className="text-center mb-15">
+        <h1 className="text-7xl font-bold mb-2">Master Your Money with</h1>
+        <h1 class="text-7xl bg-gradient-to-r from-[#22C55E] to-[#15803D] bg-clip-text text-transparent font-bold">
           FinVue
         </h1>
       </div>
 
       <div className="text-center">
-        <button className="bg-[#16A34A] text-3xl w-70 h-25 p-3 text-white rounded-3xl font-bold mr-4 shadow-2xl shadow-[#16A34A]">
+        <button className="bg-[#16A34A] text-2xl w-48 h-15 p-3 text-white rounded-2xl font-bold mr-5 shadow-2xl shadow-[#16A34A]">
           Get Started
         </button>
-        <button className="bg-[#FFFFFF] text-3xl w-70 h-25 p-3 border-1 border-[#E2E8F0] text-black rounded-3xl font-bold">
+        <button className="bg-[#FFFFFF] text-2xl w-48 h-15 p-3 border-1 border-[#E2E8F0] text-black rounded-2xl font-bold">
           Watch Demo
         </button>
       </div>
