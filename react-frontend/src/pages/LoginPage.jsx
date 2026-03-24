cecc1b96488ba97c8e60ceaf511f4decd640b7d6
Daniil <daniil.batiuskov@gmail.com>
2026-03-24 11:21:11 +0200

    fixed login page

--- react-frontend/src/pages/LoginPage.jsx
+++ react-frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router";
 import { useAuth } from "../contexts/AuthContext"; // Import the hook
 import "../styles/LoginPageStyle.css";
 
