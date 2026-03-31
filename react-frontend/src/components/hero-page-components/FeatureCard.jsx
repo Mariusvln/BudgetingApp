41276b13f9316744eb61d59a8257b4242da04f50
Daniil <daniil.batiuskov@gmail.com>
2026-03-31 10:28:25 +0300

    Created HeroPage Navigation

--- react-frontend/src/components/hero-page-components/FeatureCard.jsx
+++ react-frontend/src/components/hero-page-components/FeatureCard.jsx
@@ -1,13 +1,13 @@
 function FeatureCard({ icon, title, description }) {
   return (
-    <div className="bg-white rounded-2xl shadow p-6">
+    <div className="bg-base-100 rounded-2xl shadow p-6 border border-[#e5e7eb]">
       <div className="w-12 h-12 bg-green-100 text-green-600 rounded-lg flex items-center justify-center mb-4 p-1">
         <div className="w-8 h-8">
           <img src={icon} alt={icon} />
         </div>
       </div>
-      <h3 className="text-lg font-semibold mb-2">{title}</h3>
-      <p className="text-gray-500 text-sm">{description}</p>
+      <h3 className="text-lg font-bold mb-2">{title}</h3>
+      <p className="text-sm">{description}</p>
     </div>
   );
 }
