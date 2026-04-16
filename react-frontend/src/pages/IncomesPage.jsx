d737ae41c65664fd55d96f328a44f06b1f366286
Daniil <daniil.batiuskov@gmail.com>
2026-04-16 23:00:32 +0300

    Added Export file to IncomesPage

--- react-frontend/src/pages/IncomesPage.jsx
+++ react-frontend/src/pages/IncomesPage.jsx
@@ -39,6 +39,10 @@ function IncomesPage() {
       <TransactionNav />
       <div className="flex-1 p-6">
         <IncomeHeader />
+
+        <div className="mb-4">
+          <ExportIncomeButton />
+        </div>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           <div className="lg:col-span-2">
 
