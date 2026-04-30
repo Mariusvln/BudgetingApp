61be60cb7e229666113c03e886a949c8c1ab12fa
Benas-Orantas <benas.orantas@stud.techin.lt>
2026-04-30 12:37:50 +0200

    error, need help

--- react-frontend/src/components/main-page-components/BalanceCard.jsx
+++ react-frontend/src/components/main-page-components/BalanceCard.jsx
@@ -1,20 +1,23 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "../../assets/styles/Dashboard.css";
 
 const BalanceCard = () => {
 
-  const [loading, setLoading] = useState(true);
   const [categories, setCategories] = useState([]);
   const [incomes, setIncomes] = useState([]);
   const [expenses, setExpenses] = useState([]);
 
-  const getCurrentMonth = () => {
-    return 
+  const getMonthStart = () => {
+    const date = new Date();
+    return new Date(date.getFullYear(), date.getMonth(), 1);
   }
 
-  const fetchBalance = useCallback(
-    async () => {
-      setLoading(true)
+  const getCurrentMonthDay = () => {
+    const date = new Date();
+    return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+  }
+
+  const fetchBalance = async () => {
       try {
         const incomeResponse = await fetch(
           `http://localhost:8080/api/app/fetchIncomes`,
@@ -22,37 +25,30 @@ const BalanceCard = () => {
         );
 
         const expenseResponse = await fetch(
-          `http://localhost:8080/api/app/fetchExpenses`
+          `http://localhost:8080/api/app/fetchExpenses`,
+          {credentials: "include"},
         )
 
         if (!incomeResponse.ok || !expenseResponse.ok) throw new Error("Network response was not okay");
-        const [incomes, expenses] = await Promise.all
-        setIncomes(incomes);
-        setExpenses(expenses)
+        const [incomesResponse, expensesResponse] = await Promise.all
+        setIncomes(incomesResponse);
+        setExpenses(expensesResponse)
       } catch (error) {
         console.error("Error fetching incomes:", error)
-      } finally {
-        setLoading(false)
       }
-    }, [])
+    }
 
-  const fetchCategories = useCallback(async () => {
-      try {
-        const response = await fetch("http://localhost:8080/api/categories", {
-          credentials: "include",
-        });
-  
-        if (!response.ok) {
-          throw new Error("Failed to fetch categories");
-        }
-  
-        const data = await response.json();
-        setCategories(Array.isArray(data) ? data : []);
-      } catch (error) {
-        console.error("Error fetching categories:", error);
-        setCategories([]);
-      }
-    }, []);
+  const calculateBalance = () => {
+    let balance = 0
+    const incomesSum = incomes.reduce((partialSum, a) => partialSum + a, 0)
+    const expensesSum = expenses.reduce((partialSum, a) => partialSum + a, 0)
+    balance = (incomesSum - expensesSum);
+    return balance;
+  }
+
+  useEffect(() => {
+    fetchBalance();
+  }, [])
 
     return (
         <div className="balance-card-bg p-[32px] min-h-[240px] flex flex-col grow-30 basis-[600px] justify-center gap-[5%]">
@@ -62,7 +58,7 @@ const BalanceCard = () => {
           </div>
           <div>
           <p className="tracking-wide semibold text-[#0F172AB2]">Total Balance</p>
-          <h1 className="bold-font text-6xl black-text tracking-tight">${incomes}</h1>
+          <h1 className="bold-font text-6xl black-text tracking-tight">${calculateBalance()}</h1>
           </div>
           <div>
           <h5 className="semibold text-lg mt-3 text-[#0F172AB2]">MONTHLY SPENDING</h5>
