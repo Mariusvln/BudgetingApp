b57075cdc4dda26ab08213bb8e08baf0a2e88011
Benas-Orantas <benas.orantas@stud.techin.lt>
2026-05-12 12:19:56 +0200

    added basic chart functionality in main page with backend

--- react-frontend/src/components/main-page-components/MonthlyGrowth.jsx
+++ react-frontend/src/components/main-page-components/MonthlyGrowth.jsx
@@ -2,11 +2,11 @@ import profitIcon from "../../assets/images/icons/profit-icon.svg";
 import { Chart as ChartJS } from "chart.js/auto";
 import { Bar } from "react-chartjs-2";
 import "../../assets/styles/Dashboard.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const MonthlyGrowth = () => {
   
-  const [monthMoney, setMonth1Money] = useState(0);
+  const [month1Money, setMonth1Money] = useState(0);
   const [month2Money, setMonth2Money] = useState(0);
   const [month3Money, setMonth3Money] = useState(0);
   const [month4Money, setMonth4Money] = useState(0);
@@ -34,37 +34,39 @@ const MonthlyGrowth = () => {
 
   const fetchMonths = async () => {
     try {
+      const ranges = getMonthsRanges();
+
       const [month1Income, month1Expense, month2Income, month2Expense, month3Income, month3Expense, month4Income, month4Expense] = await Promise.all([
         fetch(
-          `http://localhost:8080/api/app/incomes/fromDateStartToDateFinish?dateStart=${getMonthsRanges[0].start}&dateEnd=${getMonthsRanges[0].end}`,
+          `http://localhost:8080/api/app/incomes/fromDateStartToDateFinish?dateStart=${ranges[0].start}&dateEnd=${ranges[0].end}`,
           { credentials: "include" },),
         fetch(
-          `http://localhost:8080/api/app/expenses/fromDateStartToDateFinish?dateStart=${getMonthsRanges[0].start}&dateEnd=${getMonthsRanges[0].end}`,
+          `http://localhost:8080/api/app/expenses/fromDateStartToDateFinish?dateStart=${ranges[0].start}&dateEnd=${ranges[0].end}`,
           { credentials: "include" },),
         fetch(
-          `http://localhost:8080/api/app/incomes/fromDateStartToDateFinish?dateStart=${getMonthsRanges[1].start}&dateEnd=${getMonthsRanges[1].end}`,
+          `http://localhost:8080/api/app/incomes/fromDateStartToDateFinish?dateStart=${ranges[1].start}&dateEnd=${ranges[1].end}`,
           { credentials: "include" },),
         fetch(
-          `http://localhost:8080/api/app/expenses/fromDateStartToDateFinish?dateStart=${getMonthsRanges[1].start}&dateEnd=${getMonthsRanges[1].end}`,
+          `http://localhost:8080/api/app/expenses/fromDateStartToDateFinish?dateStart=${ranges[1].start}&dateEnd=${ranges[1].end}`,
           { credentials: "include" },),
         fetch(
-          `http://localhost:8080/api/app/incomes/fromDateStartToDateFinish?dateStart=${getMonthsRanges[2].start}&dateEnd=${getMonthsRanges[2].end}`,
+          `http://localhost:8080/api/app/incomes/fromDateStartToDateFinish?dateStart=${ranges[2].start}&dateEnd=${ranges[2].end}`,
           { credentials: "include" },),
         fetch(
-          `http://localhost:8080/api/app/expenses/fromDateStartToDateFinish?dateStart=${getMonthsRanges[2].start}&dateEnd=${getMonthsRanges[2].end}`,
+          `http://localhost:8080/api/app/expenses/fromDateStartToDateFinish?dateStart=${ranges[2].start}&dateEnd=${ranges[2].end}`,
           { credentials: "include" },),
         fetch(
-          `http://localhost:8080/api/app/incomes/fromDateStartToDateFinish?dateStart=${getMonthsRanges[3].start}&dateEnd=${getMonthsRanges[3].end}`,
+          `http://localhost:8080/api/app/incomes/fromDateStartToDateFinish?dateStart=${ranges[3].start}&dateEnd=${ranges[3].end}`,
           { credentials: "include" },),
         fetch(
-          `http://localhost:8080/api/app/expenses/fromDateStartToDateFinish?dateStart=${getMonthsRanges[3].start}&dateEnd=${getMonthsRanges[3].end}`,
+          `http://localhost:8080/api/app/expenses/fromDateStartToDateFinish?dateStart=${ranges[3].start}&dateEnd=${ranges[3].end}`,
           { credentials: "include" },)
       ])
 
       if(!month1Income.ok || !month1Expense.ok || !month2Income.ok || !month2Expense.ok || !month3Income.ok || !month3Expense.ok || !month4Income.ok || !month4Expense.ok) {
         throw new Error("Network response was not okay")
       }
-      const [month1Incomes, month1Expenses, month2Incomes, month2Expenses, month3Incomes, month3Expenses, month4Incomes, Month4Expenses] = await Promise.all([
+      const [month1Incomes, month1Expenses, month2Incomes, month2Expenses, month3Incomes, month3Expenses, month4Incomes, month4Expenses] = await Promise.all([
         month1Income.json(),
         month1Expense.json(),
         month2Income.json(),
@@ -75,23 +77,56 @@ const MonthlyGrowth = () => {
         month4Expense.json(),
       ])
       const month1Savings = month1Incomes.reduce(
-        (partialSum, a) => partialSum + a.ammount,
+        (partialSum, a) => partialSum + a.amount,
+        0
+      ) - month1Expenses.reduce(
+        (partialSum, a) => partialSum + a.amount,
         0
       );
+      const month2Savings = month2Incomes.reduce(
+        (partialSum, a) => partialSum + a.amount,
+        0
+      ) - month2Expenses.reduce(
+        (partialSum, a) => partialSum + a.amount,
+        0
+      );
+      const month3Savings = month3Incomes.reduce(
+        (partialSum, a) => partialSum + a.amount,
+        0
+      ) - month3Expenses.reduce(
+        (partialSum, a) => partialSum + a.amount,
+        0
+      );
+      const month4Savings = month4Incomes.reduce(
+        (partialSum, a) => partialSum + a.amount,
+        0
+      ) - month4Expenses.reduce(
+        (partialSum, a) => partialSum + a.amount,
+        0
+      );
+      setMonth1Money(Math.max(0, month1Savings));
+      setMonth2Money(Math.max(0, month2Savings));
+      setMonth3Money(Math.max(0, month3Savings));
+      setMonth4Money(Math.max(0, month4Savings));
+    } catch (error) {
+      console.log("Error fetching Monthly Growth data:", error);
+      setMonth1Money(0);
+      setMonth2Money(0);
+      setMonth3Money(0);
+      setMonth4Money(0);
     }
   }
+
+  useEffect(() => {
+    (getMonthsRanges(), fetchMonths());
+  }, [])
 
   const chartData = {
     labels: ["Month 1", "Month 2", "Month 3", "This Month"],
     datasets: [
       {
-        label: "Last month saved",
-        data: [50, 200, 100, 300],
-        backgroundColor: ["#13EC6D33"],
-      },
-      {
-        label: "This month saved",
-        data: [100, 250, 150, 300],
+        label: "Money saved",
+        data: [month1Money, month2Money, month3Money, month4Money],
         backgroundColor: ["#13EC6D"],
       },
     ],
