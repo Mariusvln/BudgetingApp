ee0a4c98ae957dea9f6905b24ecfa9332d378fe4
Daniil <daniil.batiuskov@gmail.com>
2026-05-21 11:10:40 +0300

    Remodel transaction page to show recent transactions and add a link to the transaction history page

--- react-frontend/src/pages/TransactionsPage.jsx
+++ react-frontend/src/pages/TransactionsPage.jsx
@@ -1,23 +1,13 @@
 import TransactionNav from "../components/TransactionNav";
-import TransactionHeader from "../components/transaction-page-components/TransactionHeader";
 import TransactionRecentTable from "../components/transaction-page-components/TransactionRecentTable";
-import TransactionAddPanel from "../components/transaction-page-components/TransactionAddPanel";
 
 function TransactionsPage() {
   return (
     <div className="transactions-page flex min-h-screen bg-base-200 lg:ml-64">
       <TransactionNav />
 
-      <div className="transactions-page__content flex-1 p-6 pb-28 lg:pb-6">
-        {/* <TransactionHeader /> */}
-
-        <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
-          <div className="lg:col-span-2">
-            <TransactionRecentTable />
-          </div>
-
-          {/* <TransactionAddPanel /> */}
-        </div>
+      <div className="transactions-page__content w-full flex-1 p-4 pb-28 sm:p-6 lg:pb-6">
+        <TransactionRecentTable />
       </div>
     </div>
   );
